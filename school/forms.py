6f93e6ca1b02c39714d3558075205b1689f0eed8
Timur Kohut <kogt55695@gmail.com>
2026-02-26 18:36:16 +0200

    Add ModelForms for Class, Uroks, Teacher, Student

--- school/forms.py
+++ school/forms.py
@@ -1,5 +1,27 @@
 from django import forms
 from .models import Grade, Schedule
+from django import forms
+from .models import Class, Uroks, Teacher, Student
+
+class ClassForm(forms.ModelForm):
+    class Meta:
+        model = Class
+        fields = ['name']
+
+class UroksForm(forms.ModelForm):
+    class Meta:
+        model = Uroks
+        fields = ['name']
+
+class TeacherForm(forms.ModelForm):
+    class Meta:
+        model = Teacher
+        fields = ['full_name', 'uroks']
+
+class StudentForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = ['full_name', 'school_class']
 
 class GradeForm(forms.ModelForm):
     class Meta:
