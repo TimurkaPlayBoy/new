2419ca13d5b8c02629d812137b260ae8e3a75810
Timur Kohut <kogt55695@gmail.com>
2026-02-26 17:55:01 +0200

    Register models in admin panel

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Uroks, Teacher, Class, Student, Schedule, Grade
 
-# Register your models here.
+
+admin.site.register(Uroks)
+admin.site.register(Teacher)
+admin.site.register(Class)
+admin.site.register(Student)
+admin.site.register(Schedule)
+admin.site.register(Grade)
