87e5e437843238f4f4c14822b418c48d77ddd947
Timur Kohut <kogt55695@gmail.com>
2026-02-26 17:54:31 +0200

    Add Schedule and Grade models

--- school/models.py
+++ school/models.py
@@ -23,3 +23,20 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
+class Schedule(models.Model):
+    uroks = models.ForeignKey(Uroks, on_delete=models.CASCADE)
+    school_class = models.ForeignKey(Class, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    day_of_week = models.CharField(max_length=20)
+    lesson_number = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.school_class} - {self.uroks}"
+
+class Grade(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    uroks = models.ForeignKey(Uroks, on_delete=models.CASCADE)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.student} - {self.value}"
