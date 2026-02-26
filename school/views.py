1fb83347da0304f388d774a5945b3398fb2a0ed4
Timur Kohut <kogt55695@gmail.com>
2026-02-26 18:07:34 +0200

    Add ViewSets for all models

--- school/views.py
+++ school/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Uroks, Teacher, Class, Student, Schedule, Grade
+from .serializers import (
+    UroksSerializer, TeacherSerializer, ClassSerializer, StudentSerializer,
+    ScheduleSerializer, GradeSerializer
+)
 
-# Create your views here.
+class UroksViewSet(viewsets.ModelViewSet):
+    queryset = Uroks.objects.all()
+    serializer_class = UroksSerializer
+
+class TeacherViewSet(viewsets.ModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class ClassViewSet(viewsets.ModelViewSet):
+    queryset = Class.objects.all()
+    serializer_class = ClassSerializer
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class ScheduleViewSet(viewsets.ModelViewSet):
+    queryset = Schedule.objects.all()
+    serializer_class = ScheduleSerializer
+
+class GradeViewSet(viewsets.ModelViewSet):
+    queryset = Grade.objects.all()
+    serializer_class = GradeSerializer
